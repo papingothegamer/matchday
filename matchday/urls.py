2c20e2bb229fe2ea2f6b872c64451071e05403be
papingothegamer <toluwanimidaramola8@gmail.com>
2026-03-27 17:19:59 +0200

    fix(routing): use include('core.urls') in main project urls

--- matchday/urls.py
+++ matchday/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('pick/', views.pick_team, name='pick_team'),
+    path('', include('core.urls')),
 ]
